baf75ad09cdf870bde28643c71accf2324aef6a2
Guido Scialfa <dev@guidoscialfa.com>
2018-12-15 15:25:00 +0100

    Introduce Unit Tests

---
+++ tests/unit/TemplateTest.php
@@ -0,0 +1,35 @@
+<?php # -*- coding: utf-8 -*-
+// phpcs:disable
+
+namespace WordPressTemplate\Tests\Unit;
+
+use WordPressTemplate\Controller;
+use WordPressTemplate\Template;
+use WordPressTemplate\ViewData;
+
+class TemplateTest extends TestCase
+{
+    public function testInstance()
+    {
+        $testee = new Template(\Mockery::mock(Controller::class));
+
+        self::assertInstanceOf(Template::class, $testee);
+    }
+
+    public function testRender()
+    {
+        $controller = \Mockery::mock(Controller::class);
+        $viewData = \Mockery::mock(ViewData::class);
+
+        $controller
+            ->shouldReceive('render')
+            ->once()
+            ->with($viewData);
+
+        $testee = new Template($controller);
+
+        $testee->render($viewData);
+
+        self::assertTrue(true);
+    }
+}
